96021cc228193271c2202df33aa5c849f1b9569f
Hackerr7767 <97411928+Hackerr7767@users.noreply.github.com>
2022-04-29 03:13:03 +0200

    UPDATE: Finally Added Text

--- usr/bin/flex/wdg/Text.rb
+++ usr/bin/flex/wdg/Text.rb
@@ -1,1 +1,14 @@
 
+module Text
+  class Text
+    def initalize(value)
+      @value = value.capitalize
+    end
+    def new
+      return "#{@value}"
+    end
+  end
+  def test
+  wd = Text.new("string")
+  end
+end
